f232407be74086431cd3ae00486ef135cf6943a6
joe <joe.lau@placecast.net>
2017-11-30 17:52:14 -0800

    took out cheep trig fn

--- observatory/src/main/scala/observatory/Visualization.scala
+++ observatory/src/main/scala/observatory/Visualization.scala
@@ -53,54 +53,9 @@ object Visualization extends SparkJob {
       val lambda2 = k.lon
 
       c = acos(sin(theta1.toRadians) * sin(theta2.toRadians) + cos(theta1.toRadians) * cos(theta2.toRadians) * cos(abs(lambda1.toRadians - lambda2.toRadians)))
-      //c = cheap_acos(cheap_sin(theta1) * cheap_sin(theta2) + cheap_cos(theta1) * cheap_cos(theta2) * cheap_cos(abs(lambda1 - lambda2)))
-
     }
     EARTH_RADIUS_KM * c
 
-  }
-
-
-  //  val TO_RADIANS = 0.01745329251
-  //  val COS_ANS = Array(1, 0.984807753, 0.9396926209, 0.8660254039, 0.7660444434, 0.6427876101, 0.5000000005, 0.342020144, 0.1736481785, 0.0000000008948966625, -0.1736481767, -0.3420201423, -0.499999999, -0.6427876087, -0.7660444422, -0.866025403, -0.9396926202, -0.9848077527, -1, -0.9848077533, -0.9396926215, -0.8660254048, -0.7660444445, -0.6427876114, -0.5000000021, -0.3420201457, -0.1736481802, -0.000000002684689987, 0.1736481749, 0.3420201406, 0.4999999974, 0.6427876073, 0.7660444411, 0.8660254021, 0.9396926196, 0.9848077524)
-  //  val ACOS_ANS = Array(0, 0.08726646255, 0.1745329251, 0.2617993877, 0.3490658502, 0.4363323128, 0.5235987753, 0.6108652379, 0.6981317004, 0.785398163, 0.8726646255, 0.9599310881, 1.047197551, 1.134464013, 1.221730476, 1.308996938, 1.396263401, 1.483529863, 1.570796326, 1.658062788, 1.745329251, 1.832595714, 1.919862176, 2.007128639, 2.094395101, 2.181661564, 2.268928026, 2.356194489, 2.443460951, 2.530727414, 2.617993877, 2.705260339, 2.792526802, 2.879793264, 2.967059727, 3.054326189, 3.141592654)
-  val COS_ANS = for (deg <- 0 to 360) yield cos(deg.toRadians)
-  val ACOS_ANS = for (x <- -100 to 100) yield acos(x / 100d)
-  val acos_index = map_to(1, -1, 0, ACOS_ANS.size)
-
-  def cheap_acos(x: Double): Double = {
-    var index = acos_index(x).toInt
-    if (index < 0) {
-      //      println("acos: offending x = " + x)
-      0
-    }
-    else if (index >= ACOS_ANS.size) {
-      ACOS_ANS.size - 1
-    }
-    else {
-      ACOS_ANS(index)
-    }
-
-  }
-
-  val cos_index = map_to(0, 180, 0, COS_ANS.size)
-
-  def cheap_cos(theta_degree: Double): Double = {
-    if (theta_degree < 0) {
-      cheap_cos(-theta_degree)
-    }
-    else if (theta_degree > 180) {
-      -1 * COS_ANS(cos_index(theta_degree % 180).toInt)
-    }
-    else {
-      COS_ANS(cos_index(theta_degree % 180).toInt)
-    }
-
-
-  }
-
-  def cheap_sin(theta_degree: Double): Double = {
-    cheap_cos(90 - theta_degree)
   }
 
 
@@ -152,11 +107,11 @@ object Visualization extends SparkJob {
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
     val location_sample_count = min(10, temperatures.size)
 
-    val distance_threshold_km = 100.0
-    val closest_locations: List[(Location, Temperature)] = get_closest_locations(temperatures, location, distance_threshold_km, location_sample_count)
+//    val distance_threshold_km = 100.0
+//    val closest_locations: List[(Location, Temperature)] = get_closest_locations(temperatures, location, distance_threshold_km, location_sample_count)
 
-    //    val distance_threshold_qk = 8
-    //    val closest_locations: List[(Location, Temperature)] = get_closest_locations_qk(temperatures, location.toQK(distance_threshold_qk), distance_threshold_qk, location_sample_count)
+    val distance_threshold_qk = 8
+    val closest_locations: List[(Location, Temperature)] = get_closest_locations_qk(temperatures, location.toQK(distance_threshold_qk), distance_threshold_qk, location_sample_count)
 
     val foo = closest_locations.find(_._1 == location)
     if (foo.isDefined) {
@@ -175,19 +130,8 @@ object Visualization extends SparkJob {
     }
 
     numerator / denominator
-    //    predictTemperatureSpark(sc.parallelize(temperatures.toList), location)
   }
 
-//  def predictTemperatureSpark(temperatures: RDD[(Location, Temperature)], location: Location): Temperature = {
-//
-//    val distance_threshold_km = 100
-//    val location_sample_count = 4
-//    val closest_locations = get_closest_locations_spark(temperatures, location, distance_threshold_km, location_sample_count)
-//
-//    val numerator = closest_locations.foldRight(0d)((data, acc) => acc + data._2 / dist_KM(data._1, location))
-//    val denominator = closest_locations.foldRight(0d)((data, acc) => acc + 1 / dist_KM(data._1, location))
-//    numerator / denominator
-//  }
 
   /** @param points Pairs containing a value and its associated color
     * @param value  The value to interpolate
